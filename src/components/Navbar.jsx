e5bf18685528dcb1083f444e32097c1933817882
Harshita <harshitachouhan.in@gmail.com>
2024-07-04 19:51:23 +0530

    location cahnge

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import react, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import { AiOutlineMenuUnfold } from "react-icons/ai";
 
